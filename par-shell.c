4a29e195cff945b7ef0253afb4d7103007df4116
DiMesq <d.mesquita285@gmail.com>
2015-10-02 08:56:27 +0100

    Creates the par-shell main program file and includes the necessary libraries.

---
+++ par-shell.c
@@ -0,0 +1,19 @@
+/* Projeto de SO */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "commandlinereader.h"
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+}
+
+
+
+
+
+
+
+
