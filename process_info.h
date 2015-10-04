3855e252884240edee74c46aeb4432f02b8e7f82
DReigada <daniel.reigada@gmail.com>
2015-10-04 15:31:03 +0100

    Change some text and variables names.

--- process_info.h
+++ process_info.h
@@ -9,37 +9,37 @@
 /* Struct with the information about a process */
 typedef struct process_info_s {
    int pid;
-   time_t starttime;
-   time_t endtime;
+   time_t startTime;
+   time_t endTime;
 }* process_info;
 
 /**
- * Initialize the item.
+ * Initialize the process.
  * Takes as inputs the process pid and its start time.
  * Returns the new process_info.
  */
-process_info createItem(int pid, time_t starttime);
+process_info createProcess(int pid, time_t startTime);
 
 /**
  * Get the PID of a process.
  * Takes the process_info as input.
  * Returns its PID. 
  */
-int getPid(process_info item);
+int getPid(process_info process);
 
 /**
  * Get the star time of a process.
  * Takes the process_info as input.
  * Returns its start time. 
  */
-time_t getStartTime(process_info item);
+time_t getStartTime(process_info process);
 
 /**
  * Get the end time of a process.
  * Takes the process_info as input.
  * Returns its end time (-1 if its end time was not set). 
  */
-time_t getEndTime(process_info item);
+time_t getEndTime(process_info process);
 
 
 /**
@@ -47,7 +47,7 @@ time_t getEndTime(process_info item);
  * Takes as input the process_info and the end time.
  * Returns nothing.
 */
-void setEndTime(process_info item, time_t endtime);
+void setEndTime(process_info process, time_t endTime);
 
 
 #endif
