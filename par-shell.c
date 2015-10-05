2e591ddcafc0de27af3c6376418cd14a3f78e7f1
DReigada <daniel.reigada@gmail.com>
2015-10-05 20:43:10 +0100

    Feat: par-shell now waits for all processes to finish and then prints some info about them

--- par-shell.c
+++ par-shell.c
@@ -15,6 +15,13 @@
 #define MAX_N_INPUT 7 // the programs executed in the par-shell are limited to 
 					  // 5 input arguments (the last entry is always set to NULL)
 
+
+//TODO MUDAR ISTO PARA ALGURES
+int compareProcesses(void* pid, void* process){ 
+	return *(pid_t*) pid ==  getPid((process_info) process);
+}
+
+
 int main(int argc, char* argv[]){
 
 	// To initiate the par-shell no input arguments are needed
@@ -51,9 +58,43 @@ int main(int argc, char* argv[]){
 		/* see what command the user typed */
 
 		// exit
-		if (strcmp(argVector[0], "exit") == 0)
-			// TODO: precisa de ser alterada esta parte de acordo com as especificações!!!
+		if (strcmp(argVector[0], "exit") == 0){
+			
+			process_info process;
+
+			printf("Waiting for all the processes to terminate\n");
+
+			while(1){
+				int status;
+				pid_t child_pid = wait(&status);
+
+				if (child_pid > 0){	// case the pid is valid 
+					process = (process_info) getSpecificQueue(processList, &child_pid, compareProcesses, 0);
+					//checks for an error on finding the element
+					if (process == NULL){
+						fprintf(stderr, "An error occurred when searching for a process in the list. Process not found.\n");
+						continue;
+					}
+					else{
+						setEndTime(process, 0); //NAO É NECESSARIO Setting it to 0 to know it was terminated. 
+						/*WIFEXITED(estado);
+						WEXITSTATUS(estado);*/
+						//TODO maybe? adicionar á info do processo o status de saida
+					}
+				}
+				else{ //TODO: caso o pid seja -1 testar se é porque já não há processos
+					/*printf("%s\n", strerror(errno));*/
+					break;
+				}
+			}
+
+			while(!isEmptyQueue(processList)){ //while the liss is not empty print the info of all processes
+				process = (process_info) getFirstQueue(processList);
+				fprintf(stdout, "Process %d terminated\n", getPid(process));
+			}
+
 			exit(EXIT_SUCCESS);
+		}
 
 		// else we assume it was given a path to a program to execute
 		pid_t child_pid = fork();
