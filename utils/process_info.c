b7545e38a5b75f5b4f378e8548b676abf20e31bb
DiMesq <d.mesquita285@gmail.com>
2015-10-23 15:22:21 +0100

    style: make shorter lines - around 80 columns max.

--- utils/process_info.c
+++ utils/process_info.c
@@ -11,7 +11,7 @@ process_info createProcessInfo(int pid, time_t startTime){
 	process_info process = (process_info) xmalloc(sizeof(struct process_info_s));
 	process -> pid = pid;
 	process -> startTime = startTime;
-	process -> endTime = -1;			//initialize the end time with -1 to know if it was set or not
+	process -> endTime = -1;			//initialize the end time with -1 
 
 	// when initializing we assume it will exit correctly
 	process -> exitCorrectly = 1; 		// 1 if exited and 0 otherwise(was killed)
