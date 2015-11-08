d7835cde504026a501e7659b790f3869d633da88
DReigada <daniel.reigada@gmail.com>
2015-11-08 22:28:04 +0000

    Remove all semaphores and replace them with a condition variable

--- includes/globalVariables.h
+++ includes/globalVariables.h
@@ -1,6 +1,6 @@
-/** 
- * File that defines the shared variables to be used by 
- * the par-shell and the monitor thread 
+/**
+ * File that defines the shared variables to be used by
+ * the par-shell and the monitor thread
  */
 #ifndef _GLOBALVARIABLES_H_
 #define _GLOBALVARIABLES_H_
@@ -9,13 +9,10 @@
 #define EXTERN /* nothing */
 #else
 #define EXTERN extern
-#endif 
+#endif
 
 // related to creating and managing threads
 #include <pthread.h>
-
-// related to creating and managing semaphores
-#include <semaphore.h>
 
 #include "QUEUE.h"
 
@@ -25,19 +22,17 @@ EXTERN int numChildren;
 // global list to store the processes
 EXTERN Queue processList;
 
-// global mutexes 
+// global mutexes
 EXTERN pthread_mutex_t queue_lock;
 EXTERN pthread_mutex_t numChildren_lock;
 
-// global semaphores
-EXTERN sem_t children_sem;    // the number of active children
-EXTERN sem_t maxChildren_sem; // how many processes can still be created
+// global condition variables
+EXTERN pthread_cond_t numChildren_cond_variable;
 
-// global variable that is TRUE while the par-shell is running and is set to 
+// global variable that is TRUE while the par-shell is running and is set to
 // False when the exit command is given
 EXTERN int par_shell_on;
 
 
 
 #endif
-
--- utils/Auxiliares.c
+++ utils/Auxiliares.c
@@ -6,7 +6,7 @@
 #include <errno.h>
 
 /**
- * Uses the same input as malloc, and has the same output, with the only 
+ * Uses the same input as malloc, and has the same output, with the only
  * difference being that it stops execution if some error occurred when
  * calling malloc.
  */
@@ -15,13 +15,13 @@ void* xmalloc (unsigned siz){
 
 	if (mem == NULL){    /* check for errors in allocating memory */
 		fprintf(stderr, "No memory in line %d of file \"%s\".\n",
-   		__LINE__, __FILE__); exit(2);} 
+   		__LINE__, __FILE__); exit(2);}
 
 	return mem;
 }
 
 /**
- * Uses the same input as pthread_mutex_lock (and no output), with the only 
+ * Uses the same input as pthread_mutex_lock (and no output), with the only
  * difference being that it stops execution if some error occurred when
  * calling pthread_mutex_lock.
  */
@@ -35,7 +35,7 @@ void mutex_lock(pthread_mutex_t* lock){
 }
 
 /**
- * Uses the same input as pthread_mutex_unlock (and no output), with the only 
+ * Uses the same input as pthread_mutex_unlock (and no output), with the only
  * difference being that it stops execution if some error occurred when
  * calling pthread_mutex_unlock.
  */
@@ -48,69 +48,10 @@ void mutex_unlock(pthread_mutex_t* lock){
 	}
 }
 
-/**
- * Uses the same input as sem_init (and no output), with the only 
- * difference being that it stops execution if some error occurred when
- * calling sem_init.
- */
-void xsem_init(sem_t* sem, int pshared, unsigned int value){
-
-	if (sem_init(sem, pshared, value) == -1){
-		fprintf(stderr, "Error initializing the semaphore: %s\n", 
-				strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-}
 
 /**
- * Uses the same input as sem_destroy (and no output), with the only 
- * difference being that it stops execution if some error occurred when
- * calling sem_destroy.
- */
-void xsem_destroy(sem_t* sem){
-
-	if (sem_destroy(sem) != 0){
-		fprintf(stderr, 
-				"Error destroying the semaphore: %s\n", 
-				strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-}
-
-
-/**
- * Uses the same input as sem_wait (and no output), with the only 
- * difference being that it stops execution if some error occurred when
- * calling sem_wait.
- */
-void xsem_wait(sem_t* sem){
-
-	if (sem_wait(sem) != 0){
-		fprintf(stderr, 
-				"Error waiting for the semaphore: %s\n", 
-				strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-}
-
-/**
- * Uses the same input as sem_post (and no output), with the only 
- * difference being that it stops execution if some error occurred when
- * calling sem_post.
- */
-void xsem_post(sem_t* sem){
-
-	if (sem_post(sem) != 0){
-		fprintf(stderr, 
-				"Error signaling a semaphore: %s\n", 
-				strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-}
-
-/**
- * Frees the memory allocated for the queue, the string 
- * in argVector and the argVector itself. 
+ * Frees the memory allocated for the queue, the string
+ * in argVector and the argVector itself.
  *
  * If mode is 1 it also prints the terminating info about all the processes
  * that were correctly endend.
@@ -120,20 +61,20 @@ void xsem_post(sem_t* sem){
 void exitFree (char **argVector, Queue processList, int mode) {
 
 	//while the list is not empty print the info of all processes
-	while (!isEmptyQueue (processList)) { 
+	while (!isEmptyQueue (processList)) {
 		process_info process = (process_info) getFirstQueue(processList);
 
 		// print the processes that terminated normally
 		if (mode && exitedCorrectly(process))
 			fprintf(stdout,
 				"Process %d terminated normally: status=%d | execution time: %ld \n",
-				getPid(process), 
-				getExitStatus(process), 
+				getPid(process),
+				getExitStatus(process),
 				getEndTime(process) - getStartTime(process));
 		// print the ones that didn't terminate normally
 		else if (mode)
-			fprintf(stdout, 
-					"Process %d terminated without calling exit.\n", 
+			fprintf(stdout,
+					"Process %d terminated without calling exit.\n",
 					getPid(process));
 		freeProcInfo(process);		//free the process info struct
 	}
@@ -145,17 +86,17 @@ void exitFree (char **argVector, Queue processList, int mode) {
 }
 
 /**
- * Initializes the thread and the mutexes. 
+ * Initializes the thread and the mutexes.
  *
  * thread_id is a pointer to a thread id.
- * Upon creation of the thread, the thread executes start_routine - start_routine 
+ * Upon creation of the thread, the thread executes start_routine - start_routine
  * must not take any input arguments.
  *
  * mutex_id_list is a list of length n_mutexes of pointers to the mutexes' ids.
  */
-void initThread (pthread_t* thread_id, 
-				 void* (*start_routine)(void*), 
-				 pthread_mutex_t* mutex_id_list[], 
+void initThread (pthread_t* thread_id,
+				 void* (*start_routine)(void*),
+				 pthread_mutex_t* mutex_id_list[],
 				 int n_mutexes) {
 
 	// init the locks
@@ -163,17 +104,17 @@ void initThread (pthread_t* thread_id,
 	for (i = 0; i < n_mutexes; i++){
 
 		if ((lock_err = pthread_mutex_init(mutex_id_list[i], NULL)) != 0)
-			fprintf(stderr, 
-					"Error creating the lock number %d: %s\n", 
+			fprintf(stderr,
+					"Error creating the lock number %d: %s\n",
 					i+1, strerror(lock_err));
 	}
-	
-	// init the thread 
+
+	// init the thread
 	int thread_err;
 	if ((thread_err = pthread_create(thread_id, NULL, start_routine, NULL)) != 0){
 		fprintf(stderr, "Error creating thread: %s\n", strerror(thread_err));
 		exit(EXIT_FAILURE);
-	}		
+	}
 }
 
 /**
@@ -187,15 +128,15 @@ void exitThread (pthread_t* thread_id, pthread_mutex_t* mutex_id_list[], int n_m
 	// waits for thread to terminate and when it does checks for errors
 	int thread_err;
 	if ((thread_err = pthread_join(*thread_id, NULL)) != 0)
-		fprintf(stderr, 
-				"Error terminating thread: %s\n", 
+		fprintf(stderr,
+				"Error terminating thread: %s\n",
 				strerror(thread_err));
 
-	// Destroys the locks 
+	// Destroys the locks
 	int lock_err, i;
 	for (i = 0; i < n_mutexes; i++){
 		if ((lock_err = pthread_mutex_destroy(mutex_id_list[i])) != 0)
-			fprintf(stderr, "Error destroying the lock number %d: %s\n", 
+			fprintf(stderr, "Error destroying the lock number %d: %s\n",
 					i+1, strerror(lock_err));
 	}
 }
@@ -209,10 +150,10 @@ void exitThread (pthread_t* thread_id, pthread_mutex_t* mutex_id_list[], int n_m
 void updateTerminatedProcess (process_info process, time_t end_time, int status){
 
 	//Store the time the process terminated
-	setEndTime(process, end_time); 
+	setEndTime(process, end_time);
 
 	//if the process exited store its exit status
-	if (WIFEXITED(status))	setExitStatus(process, WEXITSTATUS(status));	
+	if (WIFEXITED(status))	setExitStatus(process, WEXITSTATUS(status));
 
 	// the process didn't exit, so set an error in the end time
 	else	setExitError(process);
@@ -221,12 +162,11 @@ void updateTerminatedProcess (process_info process, time_t end_time, int status)
 /**
  * Determines if two processes are the same.
  * Two processes are the same if they have the same pid.
- * Takes as input a pointer to the pid and the pointer to a process. 
+ * Takes as input a pointer to the pid and the pointer to a process.
  * Returns 1 if it is the same process and 0 otherwise.
- */ 
-int compareProcesses (void* pid, void* process){ 
+ */
+int compareProcesses (void* pid, void* process){
 
 	return *(pid_t*) pid ==  getPid((process_info) process);
 
 }
-
