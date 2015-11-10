89d7f595a1099377269e9b96a24288253b97d609
DReigada <daniel.reigada@gmail.com>
2015-11-10 23:31:34 +0000

    Style: Remove the word global from all comments int GlobalVariables

--- includes/globalVariables.h
+++ includes/globalVariables.h
@@ -18,20 +18,20 @@
 
 #include "QUEUE.h"
 
-// global variable to store the number of active children
+// variable to store the number of active children
 EXTERN int numChildren;
 
-// global list to store the processes
+// list to store the processes
 EXTERN Queue processList;
 
-// global mutexes
+// mutexes
 EXTERN pthread_mutex_t queue_lock;
 EXTERN pthread_mutex_t numChildren_lock;
 
-// global condition variables
+// condition variables
 EXTERN pthread_cond_t numChildren_cond_variable;
 
-// global variable that is TRUE while the par-shell is running and is set to
+// variable that is TRUE while the par-shell is running and is set to
 // False when the exit command is given
 EXTERN int par_shell_on;
 
