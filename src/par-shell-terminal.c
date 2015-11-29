d8fe3fb15a2453c71248ece253193282bea9cc43
DReigada <daniel.reigada@gmail.com>
2015-11-29 17:14:12 +0000

    Add the par-shell-terminal and redirect its output to a pipe

---
+++ src/par-shell-terminal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "Auxiliares.h"
+
+
+// the number of arguments of the par-shell-terminal
+#define NARGS 2
+
+
+int main(int argc, char const *argv[]) {
+
+  // To initiate the par-shell-terminal the name of the pipe for output is needed
+  if (argc != NARGS) {
+    printf("Usage: %s <par-shel pipe>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  // close the stdout and redirect the outputs to the par-shell pipe
+  xclose(1);
+  xopen(argv[1], O_WRONLY, 0666); //TODO 2 arguments
+  return 0;
+}
