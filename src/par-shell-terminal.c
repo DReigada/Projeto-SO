1de944499e20ef494f7846c352aea3f0bb809c23
DReigada <daniel.reigada@gmail.com>
2015-12-01 18:39:05 +0000

    Implement all the stats command in par-shell-terminal

--- src/par-shell-terminal.c
+++ src/par-shell-terminal.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <libgen.h>
 
 #include "Auxiliares.h"
 #include "Auxiliares-terminal.h"
@@ -17,7 +18,10 @@
 #define START_MESSAGE "\a start %d"
 #define EXIT_MESSAGE "\a exit %d"
 #define EXIT_GLOBAL_MESSAGE "\a exit-global %d"
-#define STATS_MESSAGE "\n stats %d"
+#define STATS_MESSAGE "\a stats %d"
+
+#define FIFO_PATH_FORMAT "%s/statsfifo-pid-%d"
+#define MAX_FIFO_NAME_SIZE 25
 
 // the number of arguments of the par-shell-terminal
 #define NARGS 2
@@ -31,7 +35,7 @@ int main(int argc, char const *argv[]) {
   }
 
   // open the par-shell pipe
-  int parshellFd = xopen(argv[1], O_WRONLY, 0666); //TODO 2 arguments
+  int parshellFd = xopen2(argv[1], O_WRONLY);
 
   // send the start message
   sendMessage(START_MESSAGE, parshellFd);
@@ -67,8 +71,31 @@ while (1) {
 
   // the stats command
   if (strcmp(line, STATS_COMMAND) == 0) {
+    // copy the content of argv1 to an aux string
+    char *argvCopy = malloc(strlen(argv[1]));
+    strcpy(argvCopy, argv[1]);
+
+    // create the input fifo in the same directory as the output fifo
+    char *fifopath = malloc(strlen(argvCopy) + MAX_FIFO_NAME_SIZE);
+    sprintf(fifopath, FIFO_PATH_FORMAT, dirname(argvCopy), getpid());
+    free(argvCopy);
+    xmkfifo(fifopath, FIFO_PERMISSIONS);
+
+    // send the message to par-shell
     sendMessage(STATS_MESSAGE, parshellFd);
-    // TODO receive the stats info and show it
+
+    // open the fifo
+    int fifofd = xopen2(fifopath, O_RDONLY);
+
+    // the numbers to get from par-shell
+    int numProcesses, totalTime;
+    read(fifofd, &numProcesses, sizeof(int)); //TODO change to xread
+    read(fifofd, &totalTime, sizeof(int));
+    // close the fifo
+    xclose(fifofd);
+
+    printf("Number of active processes: %d\n", numProcesses);
+    printf("Total execution time: %d\n", totalTime);
     continue;
   }
 
