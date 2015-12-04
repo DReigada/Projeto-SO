b8db0a099ced2ecee8f700b0b37fc3913b7da6a3
DiMesq <d.mesquita285@gmail.com>
2015-12-04 19:48:53 +0000

    fix bug: non-void functions weren't returning anything.

--- utils/Auxiliares.c
+++ utils/Auxiliares.c
@@ -77,6 +77,8 @@ void xfflush(FILE *stream){
 /**
  * Uses the same input as open but if some error occurs when calling open
  * it does not return null, instead it stops the execution.
+ *
+ * The function to be used when opening a regular type file.
  * Returns the file descriptor in success.
  */
 int xopen3(const char *pathname, int flags, mode_t mode){
@@ -97,6 +99,9 @@ int xopen3(const char *pathname, int flags, mode_t mode){
 /**
  * Uses the same input as open but if some error occurs when calling open
  * it does not return null, instead it stops the execution.
+ *
+ * Its the function to be used when opening a fifo type file.
+ * Returns the file descriptor in success.
  */
 int xopen2(const char *pathname, int flags){
   int fd;
@@ -130,8 +135,9 @@ void xclose(int fd){
  * Returns the  number  of bytes that were written.
  */
 ssize_t xwrite(int fd, const void *buf, size_t count){
+  ssize_t n;
   while(1){
-    if (write(fd, buf, count) == -1) {
+    if ((n = write(fd, buf, count)) == -1) {
       // if the error was EINTR read again
       if (errno == EINTR) continue;
       fprintf(stderr, "Error writing to file/pipe %s\n", strerror(errno));
@@ -139,6 +145,7 @@ ssize_t xwrite(int fd, const void *buf, size_t count){
     }
     break;
   }
+  return n;
 }
 
 /**
@@ -147,15 +154,17 @@ ssize_t xwrite(int fd, const void *buf, size_t count){
  * Returns the number of bytes that were read.
  */
 ssize_t xread(int fd, void *buf, size_t count){
+  ssize_t n;
   while(1){
-    if (read(fd, buf, count) == -1) {
-      // if the error was EINTR read again
+    if ((n = read(fd, buf, count)) == -1) {
+      // if the error was caused by a signal let it go back to the caller
       if (errno == EINTR) continue;
       fprintf(stderr, "Error reading from file/pipe %s\n", strerror(errno));
       exit(EXIT_FAILURE);
     }
     break;
   }
+  return n;
 }
 
 /**
