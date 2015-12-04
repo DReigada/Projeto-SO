93d1636140da3744969a54f8bc5bd7cbb200719c
DReigada <daniel.reigada@gmail.com>
2015-12-04 21:54:14 +0000

    Fix getLine to test the return value of get fgets instead of str

--- includes/getLine.h
+++ includes/getLine.h
@@ -5,9 +5,12 @@
  * it restricts the input stream to stdin and tests if the string that was read
  * is NULL and if there was overflow.
  *
- * Returns 1 if successful; 0 if there was no input or an error ocurred using 
+ * Returns 1 if successful; 0 if there was no input or an error ocurred using
  * fgets; -1 if there was overflow
  */
+#ifndef _GETLINE_H_
+#define _GETLINE_H
+
 int getLine(char* str, int sz);
 
-
+#endif
--- utils/getLine.c
+++ utils/getLine.c
@@ -3,27 +3,26 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "getLine.h"
 
 /**
  * It is the same as the fgets (it uses it) with the only difference being that
  * it restricts the input stream to stdin and tests if the string that was read
  * is NULL and if there was overflow.
  *
- * Returns 1 if successful; 0 if there was no input or an error ocurred using 
+ * Returns 1 if successful; 0 if there was no input or an error ocurred using
  * fgets; -1 if there was overflow
  */
 int getLine(char* str, int sz){
   char c;
 
-  fgets(str, sz, stdin);
-
-  if (str == NULL){
+  if (fgets(str, sz, stdin) == NULL){
     return 0;
   }
 
   int len = strlen(str);
 
-  //check if there was overflow 
+  //check if there was overflow
   if (str[len - 1] != '\n'){
 
     int feedback = 1;
